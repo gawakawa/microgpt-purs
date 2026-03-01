8bb8a214b64a4765876810e0d9d13b96d6a5387f
gawakawa <mashurakiryu@icloud.com>
2026-03-01 17:02:31 +0900

    💡 Add documentation comments to Tokenizer functions

--- src/Tokenizer.purs
+++ src/Tokenizer.purs
@@ -16,18 +16,23 @@ derive instance Newtype Token _
 derive instance Eq Token
 derive newtype instance Show Token
 
+-- | Build sorted unique character array from strings
 buildVocab :: Array String -> Array Char
 buildVocab = sort <<< nub <<< concatMap toCharArray
 
+-- | Beginning of Sequence token (word boundary marker)
 bos :: Token
 bos = Token $ on (-) toCharCode 'z' 'a' + 1
 
+-- | Convert character to token ('a' → 0, 'b' → 1, ...)
 encode :: Char -> Token
 encode c = Token $ on (-) toCharCode c 'a'
 
+-- | Convert token back to character (Nothing for BOS)
 decode :: Token -> Maybe Char
 decode bos = Nothing
 decode (Token tok) = fromCharCode $ tok + toCharCode 'a'
 
+-- | Convert strings to token sequence, surrounding each with BOS
 tokenize :: Array String -> Array Token
 tokenize docs = surroundMap [ bos ] (map encode <<< toCharArray) docs
